5f483027b0da885f08df3b6a1758d0991b740b4c
phuid <simon.hrouda@gmail.com>
2023-12-21 12:20:28 +0100

    works

--- c.cpp
+++ c.cpp
@@ -3,28 +3,38 @@
 #include "zmq.hpp"
 #include "zmq_addon.hpp"
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 int main(void)
 {
     //  Prepare our context and updates socket
     zmq::context_t ctx(1);
+
     zmq::socket_t subscriber(ctx, zmq::socket_type::sub);
     subscriber.connect("tcp://localhost:5556");
     subscriber.set(zmq::sockopt::subscribe, "");
+
+    zmq::socket_t command_sender(ctx, zmq::socket_type::pub);
+    command_sender.connect("tcp://localhost:5557");
 
     std::cout << "start done" << std::endl;
 
     while (true)
     {
-        // while rcv(zmq::rec_flags::no wait) receive server output
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
 
-        std::vector<zmq::message_t> recv_msgs;
-        zmq::recv_result_t result =
-            zmq::recv_multipart(subscriber, std::back_inserter(recv_msgs));
-        assert(result && "recv failed");
-        assert(*result == 2);
+        zmq::message_t received;
+        while(subscriber.recv(received, zmq::recv_flags::dontwait)) {
+            std::cout << "received: \"" << received.to_string() << "\"" << std::endl;
+        }
 
-        std::cout << recv_msgs[0].to_string() << std::endl;
+        // read user input
+        std::string u_in;
+        getline(std::cin, u_in);
+
+        zmq::message_t msg(u_in.c_str(), u_in.length());
+        command_sender.send(msg, zmq::send_flags::none);
     }
     return 0;
 }
--- s.cpp
+++ s.cpp
@@ -7,36 +7,32 @@
 #include <iostream>
 #include <string>
 
-int main (void)
+int main(void)
 {
     //  Prepare our context and publisher socket
     zmq::context_t ctx(1);
-    zmq::socket_t publisher(ctx, zmq::socket_type::pub);
-    zmq::socket_t command_receiver(ctx, zmq::socket_type::sub);
-    publisher.bind("tcp://*:5556");
 
-    std::cout << "start done" << std::endl;
+    zmq::socket_t publisher(ctx, zmq::socket_type::pub);
+    publisher.bind("tcp://*:5556");
+    
+    zmq::socket_t command_receiver(ctx, zmq::socket_type::sub);
+    command_receiver.bind("tcp://*:5557");
+    command_receiver.set(zmq::sockopt::subscribe, "");
 
     uint64_t wahoo = 0;
+    std::cout << "start done" << std::endl;
 
-    while (true) {
-        zmq::multipart_t received;
-        zmq::recv_multipart(command_receiver, received, zmq::recv_flags::none);
+    while (true)
+    {
+        zmq::message_t received;
+        command_receiver.recv(received, zmq::recv_flags::none);
 
-        std::cout << "received parts:";
-        for (auto &&msg : received)
-        {
-            std::cout << msg.str() << ", ";
-        }
-        std::cout << std::endl;
-        
+        std::cout << "received: \"" << received.to_string() << "\"" << std::endl;
 
-        zmq::multipart_t mp; //will it destruct?
-        mp.addstr("SUCCESS");
-        mp.addstr("received command \"" + received_cmd_str + "\"");
+        std::string msg_string = "nice, thank you bro, i got this from you \"" + received.to_string() + "\"";
 
-        publisher.send(mp, zmq::send_flags::none);
-        // mp.clear();
+        zmq::message_t msg(msg_string.c_str(), msg_string.length() + 1);
+        publisher.send(msg, zmq::send_flags::none);
 
         wahoo++;
     }
